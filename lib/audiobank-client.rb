45924460530f5cd00391c5e18d073b104e2f4aa6
Alban Peignier <alban@tryphon.eu>
2013-07-17 10:37:41 +0200

    Support old ActiveSupport ...

--- lib/audiobank-client.rb
+++ lib/audiobank-client.rb
@@ -6,12 +6,17 @@ require 'json'
 require "virtus"
 
 require "null_logger"
-require "active_support/core_ext/module/attribute_accessors"
-require "active_support/core_ext/class/attribute_accessors"
 
-require "active_support/core_ext/module/delegation"
-require "active_support/json"
-require "active_support/core_ext/object/to_json"
+begin
+  require "active_support/core_ext/module/attribute_accessors"
+  require "active_support/core_ext/class/attribute_accessors"
+
+  require "active_support/core_ext/module/delegation"
+  require "active_support/json"
+  require "active_support/core_ext/object/to_json"
+rescue LoadError
+  require "active_support"
+end
 
 require 'net/ftp'
 
